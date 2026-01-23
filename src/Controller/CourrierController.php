035394469222431ae9f97d16affc8ef8812a1a75
Yassine KHLOUFI <ykhloufi@jehann.fr>
2026-01-23 11:52:15 +0100

    feat: Implement admin dashboard UI and configure security access

---
+++ src/Controller/CourrierController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CourrierController extends AbstractController
+{
+    #[Route('/courrier/new', name: 'app_courrier_new')]
+    public function new(): Response
+    {
+        return $this->render('courrier/new.html.twig');
+    }
+
+    #[Route('/courriers', name: 'app_courrier_list')]
+    public function list(): Response
+    {
+        $courriers = [];
+        return $this->render('courrier/list.html.twig', [
+            'courriers' => $courriers
+        ]);
+    }
+
+    #[Route('/courrier/{id}', name: 'app_courrier_show', methods: ['GET'])]
+    public function show(): Response
+    {
+        return $this->render('courrier/show.html.twig');
+    }
+
+    #[Route('/courrier/{id}/edit', name: 'app_courrier_edit')]
+    public function edit(): Response
+    {
+        return $this->render('courrier/edit.html.twig');
+    }
+
+    #[Route('/courrier/{id}', name: 'app_courrier_delete', methods: ['POST'])]
+    public function delete(): Response
+    {
+        return $this->redirectToRoute('app_courrier_list');
+    }
+}
--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class DashboardController extends AbstractController
 {
-    #[Route('/', name: 'app_home')] // Notre route, le GPS de l'application
+    #[Route('/dashboard', name: 'app_dashboard')] // Notre route, le GPS de l'application
     public function index(): Response // Notre action, le point de départ du chef d'orchestre
     {
         // Le contrôleur délègue la présentation de Twig via la méthode render()
-        return $this->render('home/index.html.twig', [
+        return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'HomeController',
             'message' => 'Bienvenue sur GECAT, votre application de gestion de courrier administratif !'
         ]);
